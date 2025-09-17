e9bed21e1b30e4e1bde9fa53b468a68325bd12da
ShashankTrevonix <shashanka@trevonix.com>
2025-09-17 10:41:51 +0530

    Update SimBasket.tsx

--- components/SimBasket.tsx
+++ components/SimBasket.tsx
@@ -153,7 +153,7 @@ export default function SimBasket({ selectedPlan, onBack, onCheckout }: SimBaske
                     </div>
                     <div className="flex justify-between">
                       <span>Delivery:</span>
-                      <span className="font-medium">Physical Delivery</span>
+                      <span className="font-medium">To your address</span>
                     </div>
                   </div>
                 </div>
@@ -226,7 +226,6 @@ export default function SimBasket({ selectedPlan, onBack, onCheckout }: SimBaske
                 ) : (
                   <div className="flex items-center justify-center space-x-2">
                     <span>Choose Payment Method</span>
-                    <ArrowRight className="w-5 h-5 flex-shrink-0" />
                   </div>
                 )}
               </Button>
